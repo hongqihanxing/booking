062dd5ce401c832c78495967ed5f13c6cbdb695e
zhq <1026491298@qq.com>
2018-04-11 01:08:12 +0800

    初始化代码

---
+++ src/main/java/com/muunet/HelloController.java
@@ -0,0 +1,15 @@
+package com.muunet;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ZH on 2018/4/11.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello,Welcome to muunet!";
+    }
+}
