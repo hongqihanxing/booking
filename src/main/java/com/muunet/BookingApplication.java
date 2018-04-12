540b0dd77c0a748d877dd861449d7c739222c5a5
zhanghongqi21 <zhanghongqi1@jd.com>
2018-04-12 10:35:55 +0800

    添加自定义配置文件

--- src/main/java/com/muunet/BookingApplication.java
+++ src/main/java/com/muunet/BookingApplication.java
@@ -2,9 +2,16 @@ package com.muunet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookingApplication {
+public class BookingApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(BookingApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingApplication.class, args);
--- src/main/java/com/muunet/HelloController.java
+++ src/main/java/com/muunet/HelloController.java
@@ -1,5 +1,7 @@
 package com.muunet;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
+    @Value("${sso.auth.cookie.domain}")
+    private String domain;
+
     @RequestMapping("/hello")
     public String hello() {
+        System.err.println(domain);
         return "Hello,welcome to muunet!";
     }
 }
